0d8cb701ff473c793fd6ae0ce3d613ac453ddf6c
Varshini V K <varshini58varsh@gmail.com>
2026-02-05 20:11:47 +0000

    Add palindrome.java

---
+++ palindrome.java
@@ -0,0 +1,20 @@
+
+import java.util.Scanner;
+
+public class palindrome {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a String:");
+        String str = sc.nextLine();
+        String rev = "";
+        for (int i = str.length() - 1; i >= 0; i--) {
+            rev = rev + str.charAt(i);
+        }
+        if (str.equals(rev)) {
+            System.out.println("The given string is a palindrome");
+        } else {
+            System.out.println("The given string is not a palindrome");
+        }
+    }
+}
